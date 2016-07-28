97bb34c876c5f5a5b3d50df00753256289fbb4ff
Ubuntu <ubuntu@ip-10-138-88-165.ec2.internal>
2016-07-28 19:13:55 +0000

    test-mult

--- test-mult.sh
+++ test-mult.sh
@@ -1,2 +1,6 @@
+echo "--- MOCKBOT COMPOSITE ---"
 time bin/mockbot composite test/blankshirt.png test/tcb_spies14x16.png 409 192 665 760 multiply test/out1.png
+echo "generated test/out1.png"
+echo "--- IMAGEMAGICK COMPOSITE ---"
 time convert -composite test/blankshirt.png test/tcb_spies14x16.png -compose multiply -alpha on -geometry 665x760+409+192 -depth 8 test/out.png
+echo "generated test/out1.png"
