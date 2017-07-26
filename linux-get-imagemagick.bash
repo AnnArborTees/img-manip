b90933aeda2400ef2a11a06dd037b40f4c9d55f7
Nigel Baillie <metreckk@gmail.com>
2017-07-26 10:53:26 -0400

    remove duplicate line in linux-get-imagemagick.bash

--- linux-get-imagemagick.bash
+++ linux-get-imagemagick.bash
@@ -2,5 +2,4 @@
 
 sudo apt-get purge graphicsmagick-libmagick-dev-compat
 sudo apt-get install libmagick++-dev
-sudo apt install libmagick++-dev
 sudo ln -s /usr/lib/x86_64-linux-gnu/ImageMagick-6.8.9/bin-Q16/Magick++-config /usr/bin/
