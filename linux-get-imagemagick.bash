0e6dc56e460592e2cc52ff3f04f04eda585391df
Nigel Baillie <metreckk@gmail.com>
2018-07-23 18:35:37 -0400

    update linux-get-imagemagick.bash

--- linux-get-imagemagick.bash
+++ linux-get-imagemagick.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get purge graphicsmagick-libmagick-dev-compat
-sudo apt-get install libmagick++-dev
-sudo ln -s /usr/lib/x86_64-linux-gnu/ImageMagick-6.9.7/bin-Q16/Magick++-config /usr/bin/
+sudo apt-get install libmagick++-dev libwebp-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/$(ls /usr/lib/x86_64-linux-gnu | grep ImageMagick | head -n1)/bin-Q16/Magick++-config /usr/bin/
