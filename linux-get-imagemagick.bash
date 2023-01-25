01203c5bb15c1e6fe947342ed451c9c3051a4a53
Ricky Winowiecki <slikstir@gmail.com>
2023-01-25 17:16:23 -0500

    Removing sudo for docker

--- linux-get-imagemagick.bash
+++ linux-get-imagemagick.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get purge graphicsmagick-libmagick-dev-compat
-sudo apt-get install libmagick++-dev libwebp-dev
-sudo ln -s /usr/lib/x86_64-linux-gnu/$(ls /usr/lib/x86_64-linux-gnu | grep ImageMagick | head -n1)/bin-Q16/Magick++-config /usr/bin/
+apt-get purge graphicsmagick-libmagick-dev-compat
+apt-get install libmagick++-dev libwebp-dev
+ln -s /usr/lib/x86_64-linux-gnu/$(ls /usr/lib/x86_64-linux-gnu | grep ImageMagick | head -n1)/bin-Q16/Magick++-config /usr/bin/
